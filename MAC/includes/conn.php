cdf5de4942be9e1cb484851816a738f763b8b46b
matelat-daw <cesarmatelat@gmail.com>
2024-04-18 09:32:05 +0100

    Update 18-04-2024 09:31

--- MAC/includes/conn.php
+++ MAC/includes/conn.php
@@ -2,7 +2,7 @@
 session_start(); // Incluyo el session_start() ya que se usará en casi todos los scripts.
 try // Intenta la conexión
 {
-	$conn = new PDO('mysql:host=localhost;dbname=macs', "root", $_ENV["MySQL"]);
+	$conn = new PDO('mysql:host=localhost;dbname=macs', "root", "");
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) // En caso de error
