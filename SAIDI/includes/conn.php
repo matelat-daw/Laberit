15eb1f590a0b552b0c2a89667331199f040efd40
matelat-daw <cesarmatelat@gmail.com>
2024-06-03 11:40:12 +0100

    Update 03-06-2024 11:40

--- SAIDI/includes/conn.php
+++ SAIDI/includes/conn.php
@@ -1,7 +1,6 @@
 <?php // Conexión con la base de datos en PDO.
-try // Intenta la conexión
+try // Intenta la conexión con MariaDB, se necesita una base de datos llamada macs con una tabla llamada mac que contiene todas las macs de todos los fabricantes.
 {
-	// $conn = new PDO('mysql:host=localhost;dbname=macs', "root", $_ENV["MySQL"]);
 	$conn = new PDO('mysql:host=localhost;dbname=macs', "root", "");
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
@@ -15,11 +14,8 @@ use InfluxDB2\Client;
 $org = 'laberit';
 $bucket = 'SAIDI_DATA';
 
-// $org = 'Laberit';
-// $bucket = 'MACDB';
-
 $client = new Client([
     "url" => "http://localhost:8086",
     "token" => $_ENV["Influx-Token"],
-]);
+]); // Para la conexión con InfluxDB es necesario el token que está en una variable de entorno, el puerto (8086), La organización que es laberit y el nombre de la base de datos(bucket) SAIDI_DATA.
 ?>
