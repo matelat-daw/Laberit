76b196d7a26e3b6bb7853e2024ef8a2988cfc479
matelat-daw <cesarmatelat@gmail.com>
2025-03-28 10:08:28 +0000

    Update 28-3-2025 10:08

--- MongoApi/obj/Debug/net8.0/MongoApi.AssemblyInfo.cs
+++ MongoApi/obj/Debug/net8.0/MongoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MongoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219386c0253622baa690dbf8f51e629502966b12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f1216718910063197a5961aac8d4aba13a9f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("MongoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MongoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
