5a675f7f507720a4b2df9bdf4a6d4f3ff0e6dada
matelat-daw <cesarmatelat@gmail.com>
2025-04-09 13:01:11 +0100

    Update 9-4-2025 13:01

---
+++ Angular/src/app/Views/Read.ts
@@ -0,0 +1,63 @@
+// import React, { useEffect, useState } from 'react';
+// import { getUsers, getImages } from '../Service/Service';
+import { getUsers } from '../Service/Service';
+import logo from './../assets/spinner.gif';
+
+const Read = () => {
+    const navegar = useNavigate();
+    const [users, setUsers] = useState([]);
+    // const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+useEffect(() => {
+    setIsLoading(true);
+    const fetchItems = async () => {
+      const users = await getUsers();
+      console.log(users);
+      // const images = await getImages(users);
+      setIsLoading(false);
+      setUsers(users);
+      // setImages(images);
+    };
+    fetchItems();
+  }, []);
+
+  if (isLoading) {
+    return <div><h2>Cargando...</h2><img src={logo} alt='Rueda Cargando'></img></div>;
+  }
+
+  return (
+    <div>
+      <h2>Lista de users con Imágenes</h2>
+      <br />
+      <button onClick={ (e) => navegar('/create')} className='btn btn-success left'>Añadir Receta</button>
+      <br /><br />
+      <table>
+        <thead>
+            {/* <tr><th>ID</th><th>Nombre</th><th>E-mail</th><th>Imagen</th><th>Acciones</th></tr> */}
+            <tr><th>ID</th><th>Nombre</th><th>E-mail</th><th>Acciones</th></tr>
+        </thead>
+        <tbody>
+                {users.map((user, i) => (
+                    // <tr key={user.id}><td>{user.id}</td><td>{user.name}</td><td>{user.email}</td><td><a href={user.image} target='_blank'><img src={images[i].image} width={120} /></a></td><td><button onClick={ (e) => navegar(`/details/${receta.id}`)} className='btn btn-primary'>Detalles</button>&nbsp;&nbsp;<button onClick={ (e) => navegar(`/create/${user.id}`)} className='btn btn-info'>Actualizar</button>&nbsp;&nbsp;<button onClick={(e)=> navegar(`/delete/${user.id}`)} className='btn btn-danger'>Eliminar</button></td></tr>
+                    <tr key={user.id}><td>{user.id}</td><td>{user.name}</td><td>{user.email}</td><td><button onClick={ (e) => navegar(`/details/${receta.id}`)} className='btn btn-primary'>Detalles</button>&nbsp;&nbsp;<button onClick={ (e) => navegar(`/create/${user.id}`)} className='btn btn-info'>Actualizar</button>&nbsp;&nbsp;<button onClick={(e)=> navegar(`/delete/${user.id}`)} className='btn btn-danger'>Eliminar</button></td></tr>
+                ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Read;
+
+function useNavigate() {
+    throw new Error('Function not implemented.');
+}
+function useState(arg0: never[]): [any, any] {
+    throw new Error('Function not implemented.');
+}
+
+function useEffect(arg0: () => void, arg1: never[]) {
+    throw new Error('Function not implemented.');
+}
+
--- MongoApi/obj/Debug/net8.0/MongoApi.AssemblyInfo.cs
+++ MongoApi/obj/Debug/net8.0/MongoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MongoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86fa64494dfc3f1033b4a7fc87cb4dfd856845ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78a87eecb3703fc725a95d431040ebc04bc25a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MongoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MongoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
