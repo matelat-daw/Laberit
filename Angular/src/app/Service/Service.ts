6f364682d71c33122e9d6947142c5602ad388426
matelat-daw <cesarmatelat@gmail.com>
2025-04-11 13:53:57 +0100

    Update 11-4-2025 13:53

--- Angular/src/app/Service/Service.ts
+++ Angular/src/app/Service/Service.ts
@@ -1,7 +1,7 @@
 import toast from './js/script.js';
 
 interface User {
-    id: number;
+    id: string;
     name: string;
     surname1: string;
     email: string;
@@ -9,12 +9,13 @@ interface User {
 }
 
 let users: User[] = [];
+let url = "https://192.168.83.41/";
 
 // Obtener Todos los Usuarios.
 export const getUsers = async (): Promise<User[] | undefined> => {
     try {
-        return await fetch("https://88.24.25.50/api/Account/Users").then(respuesta => respuesta.json())
-        // return await fetch("https://localhost:7227/api/Account/Users").then(respuesta => respuesta.json())
+        // return await fetch("https://88.24.25.50/api/Account/Users").then(respuesta => respuesta.json())
+        return await fetch(url + "api/Account/Users").then(respuesta => respuesta.json())
         .catch(respuesta => toast(2, "Error de Conexión", "Lo Siento No hay Conexión con el Servidor. Asegurate de que el Servidor está en Ejecución. Error: " + respuesta))
         // .then(jsonData => getImages(jsonData));
     } catch (error) {
@@ -49,23 +50,19 @@ function createElements(blobImg: Blob): HTMLImageElement[] {
 }
   
 // Agregar/Modificar un Dato.
-export const createUser = async (id: User | null, newUser: User): Promise<User[] | undefined> => {
-    if (id.id) {
+export const createUser = async (id: string, newUser: User): Promise<User[] | undefined> => {
+    if (id) {
         try {
-            users = users.map(user => 
-                user.id === id.id ? { ...user, ...newUser } : user
+            users = users.map(user => user.id === id ? { ...user, ...newUser } : user
             );
-            console.log("Usuario Modificado: " + id.id);
-            console.log("Usuario con Nombre: " + id.name);
 
             let datosUpdate = {
-              id: id.id,
-              nombre: id.name,
-              surname1: id.surname1,
-              email: id.email,
-              profileImage: id.profileImage
+              nombre: newUser.name,
+              surname1: newUser.surname1,
+              email: newUser.email,
+              profileImage: newUser.profileImage
           }
-          fetch("https://88.24.25.50/api/Account/Update/" + id.id, {
+          fetch(url + "api/Account/Update/" + id, {
               method: "PATCH",
               body: JSON.stringify(datosUpdate),
               headers: {
@@ -82,12 +79,12 @@ export const createUser = async (id: User | null, newUser: User): Promise<User[]
     } else {
         try {
             let datosCreate = {
-                nombre: id.name,
-                surname1: id.surname1,
-                email: id.email,
-                profileImage: id.profileImage
+                nombre: newUser.name,
+                surname1: newUser.surname1,
+                email: newUser.email,
+                profileImage: newUser.profileImage
             }
-            fetch("https://88.24.25.50/api/Account/Register", {
+            fetch(url + "api/Account/Register", {
                 method: "POST",
                 body: JSON.stringify(datosCreate),
                 headers: {
@@ -109,7 +106,7 @@ export const createUser = async (id: User | null, newUser: User): Promise<User[]
 export const deleteUser = async (id: string): Promise<void> => {
     try {
         users = users.filter(user => user.id !== id);
-        fetch("https://88.24.25.50/api/Account/Delete/" + id, {
+        fetch(url + "api/Account/Delete/" + id, {
           method: "DELETE",
           body: JSON.stringify(id),
           headers: {
